e72fe57ce5612984b5c42f1fcb10a7c3136ae40a
Reuben <reuben.eggar@me.com>
2019-04-30 21:05:49 +0100

    🐛

--- app/src/store/middleware/index.js
+++ app/src/store/middleware/index.js
@@ -1,6 +1,4 @@
 import {applyMiddleware, compose} from 'redux'
 
-export default compose(
-  applyMiddleware(),
-  typeof window === 'object' && window.__REDUX_DEVTOOLS_EXTENSION__ ? window.__REDUX_DEVTOOLS_EXTENSION__() : f => f
-)
+const composeEnhancers = window.__REDUX_DEVTOOLS_EXTENSION_COMPOSE__ || compose
+export default composeEnhancers(applyMiddleware())
