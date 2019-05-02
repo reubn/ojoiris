12e990e9873617da5d06f67ed449d83e623b4cd1
Reuben <reuben.eggar@me.com>
2019-05-02 23:11:14 +0100

    🐛 Hide Play Button

--- app/src/components/QRScanner/index.js
+++ app/src/components/QRScanner/index.js
@@ -1,4 +1,4 @@
-import React, {createRef, useEffect, useCallback} from 'react'
+import React, {useRef, useEffect, useCallback} from 'react'
 import {useDispatch, useMappedState} from 'redux-react-hook'
 
 import parseQRCode from '../../store/actions/parseQRCode'
@@ -10,7 +10,7 @@ import QrScanner from '../../QrScanner'
 import {container, overlay, instructions, video, pulseSuccess, pulseScanning, pulseFail} from './style'
 
 export default () => {
-  const videoRef = createRef()
+  const videoRef = useRef()
   const dispatch = useDispatch()
 
   const mapState = useCallback(state => ({
@@ -26,10 +26,10 @@ export default () => {
   })[status]
 
   useEffect(() => {
-    const tmp = videoRef.current.play
-
-    videoRef.current.play = () => true
-    videoRef.current.specialPlay = tmp
+    // const tmp = videoRef.current.play
+    //
+    // videoRef.current.play = () => true
+    // videoRef.current.specialPlay = tmp
 
     const qrScanner = new QrScanner(videoRef.current, result => parseQRCode(dispatch, result))
 
@@ -44,7 +44,7 @@ export default () => {
       <style>{`.${overlay}:after{animation-name: ${overlayAnimation}}`}</style>
         <p className={instructions}>{instruction}</p>
       </section>
-      <video ref={videoRef} muted autoPlay playsInline className={video} />
+      <video ref={videoRef} muted playsInline className={video} />
     </section>
 )
 }
