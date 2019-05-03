28e17ffb272be0f2d9686369c6cf915d1dd7bb06
Reuben <reuben.eggar@me.com>
2019-05-03 11:55:28 +0100

    🐛

--- app/src/components/LightControlScreen/ColourPicker/index.js
+++ app/src/components/LightControlScreen/ColourPicker/index.js
@@ -1,4 +1,4 @@
-import React, {useRef, useEffect, useState, useMemo} from 'react'
+import React, {useRef, useEffect, useState} from 'react'
 
 import calculateSizing from './calculateSizing'
 import touchHandler from './touchHandler'
@@ -14,10 +14,11 @@ export default ({hue: hueProp=0, onChange}) => {
   const [hue, setHue] = useState(hueProp)
   const [portalPosition, setPortalPosition] = useState({left: null, bottom: null})
 
-  const handleTouch = touchHandler({outerCircleRef, innerCircleRef, portalRef, setHue})
-
+  useEffect(() => setHue(hueProp), [hueProp])
   useEffect(syncHueToPortal({outerCircleRef, innerCircleRef, hue, setPortalPosition, onChange}), [hue])
   useEffect(keepStill(containerRef), [])
+
+  const handleTouch = touchHandler({outerCircleRef, innerCircleRef, portalRef, setHue})
 
   return (
     <>
