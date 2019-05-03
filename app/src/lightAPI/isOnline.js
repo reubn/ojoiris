80fa70c8e819563800a080e4650621a57489bb47
Reuben <reuben.eggar@me.com>
2019-05-03 10:43:28 +0100

    ✨ Better Timeouts

--- app/src/lightAPI/isOnline.js
+++ app/src/lightAPI/isOnline.js
@@ -2,7 +2,7 @@ import makeRequest from './makeRequest'
 
 export default async ({id}) => {
   const request = makeRequest({}, {id}).catch(() => ({ok: false}))
-  const timeout = new Promise(r => setTimeout(r, 2000)).then(() => ({ok: false}))
+  const timeout = new Promise(r => setTimeout(r, 3000)).then(() => ({ok: false}))
 
   const fastest = await Promise.race([request, timeout])
 
--- app/src/store/actions/lightState.js
+++ app/src/store/actions/lightState.js
@@ -3,6 +3,6 @@ import debounce from 'debounce-fn'
 const debounced = debounce((dispatch, localState) => {
   dispatch({type: 'LIGHT_STATE', payload: localState})
   debounced.cancel()
-}, {wait: 100})
+}, {wait: 40, leading: true})
 
 export default debounced
