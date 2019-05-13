95d9a4d41729a01b6a1739d6d9c55a5fe16d98e5
Reuben <reuben.eggar@me.com>
2019-05-13 08:59:51 +0100

    🐛 Persist when light is not found also

--- app/src/store/middleware/persistState.js
+++ app/src/store/middleware/persistState.js
@@ -3,7 +3,7 @@ import {write} from  '../../localStorage'
 export default ({getState}) => next => action => {
   next(action)
 
-  if(action.type.startsWith('LIGHT_STATE')){
+  if(action.type.startsWith('LIGHT_')){
       const {light: {state}, metadata: {id}} = getState()
       write({id, state})
   }
