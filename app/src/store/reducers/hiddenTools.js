87b5b596902495d6185b0925a559feb92e28a1da
Reuben <reuben.eggar@me.com>
2019-05-25 12:03:14 +0100

    ✨ Toggle Tools

--- app/src/store/reducers/hiddenTools.js
+++ app/src/store/reducers/hiddenTools.js
@@ -1,7 +1,7 @@
 import inital from '../initials/hiddenTools'
 
 export default (state=inital, {type, payload, ...action}) => {
-  if(type === 'HIDDEN_TOOLS') return payload !== undefined ? payload : true
+  if(type === 'HIDDEN_TOOLS') return payload !== undefined ? payload : !state
 
   return state
 }
