187b31c4d8abd2846cefadf665c7f453359d6858
Reuben <reuben.eggar@me.com>
2019-05-26 08:44:59 +0100

    🐛

--- app/src/store/actions/parseQRCode.js
+++ app/src/store/actions/parseQRCode.js
@@ -4,12 +4,24 @@ const sleep = m => new Promise(r => setTimeout(r, m))
 
 const pattern = /^WIFI:S:Ojoiris-(?<id>[A-Z]{2}[0-9]{2}).*?P:(?<password>.+?);;(?<key>.+?),(?<mac>.+?)$/
 
+let failTimer
+let failLocked = false
+
 export default async (dispatch, result) => {
+  if(failLocked) return dispatch({type: 'QR_SCAN_FAIL'})
+
   if(!result) return dispatch({type: 'QR_SCAN_SCANNING'})
 
   const {data, location} = result
   const match = pattern.exec(data)
-  if(!match) return dispatch({type: 'QR_SCAN_FAIL'})
+  if(!match) {
+    failLocked = true
+
+    clearTimeout(failTimer)
+    failTimer = setTimeout(() => {failLocked = false}, 1500)
+
+    return dispatch({type: 'QR_SCAN_FAIL'})
+  }
 
   dispatch({type: 'QR_SCAN_SUCCESS'})
 
